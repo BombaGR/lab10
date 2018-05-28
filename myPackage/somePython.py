79ea08906a7d7fb890f60331112c4961336b7a07
Mateusz Wojcik <w.matiu@gmail.com>
2018-05-28 11:40:18 +0200

    init

---
+++ myPackage/somePython.py
@@ -0,0 +1,83 @@
+import json
+import os
+
+recipies_path = 'porady'
+
+
+def recipies_load_from_file():
+    if os.path.exists(recipies_path):
+        with open(recipies_path, 'r') as file:
+            recipies = json.load(file)
+        return recipies
+    else:
+        return []
+
+
+def recipies_save(recipies):
+    with open(recipies_path, 'w') as file:
+        json.dump(recipies, file)
+
+
+def save_rece():
+    recipies_save(recipies)
+
+
+curr_recipie = 0
+recipies = recipies_load()
+
+
+def recipie_search():
+    if recipies:
+        pattern = input('Podaj tytuł: ')
+        for recipie in recipies:
+            if pattern.lower() in recipie['tytul'].lower():
+                print('Tytuł: {}'.format(recipie['tytul']))
+                print('Opis: {}'.format(recipie['opis']))
+
+
+def recipie_print(index, recipies):
+    if recipies:
+        print('Tytuł: {}'.format(recipies[index]['tytul']))
+        print('Opis: {}'.format(recipies[index]['opis']))
+
+
+def fun():
+    print("")
+
+
+def recipies_show():
+    if recipies:
+        for recipie in recipies:
+            print('Tytuł: {}'.format(recipie['tytul']))
+            print('Opis: {}'.format(recipie['opis']))
+
+
+def recipie_add():
+    new = {}
+    new['tytul'] = input('Tytuł: ')
+    new['opis'] = input('Opis: ')
+    recipies.append(new)
+
+
+def recipie_remove():
+    global curr_recipie
+    if -1 < curr_recipie < len(recipies):
+        del recipies[curr_recipie]
+
+
+def recipie_nextt():
+    global curr_recipie
+    curr_recipie += 1
+    if curr_recipie >= len(recipies):
+        curr_recipie = len(recipies) - 1
+    print('Aktualna porada:')
+    recipie_print(curr_recipie, recipies)
+
+
+def recipie_prev():
+    global curr_recipie
+    curr_recipie -= 1
+    if curr_recipie < 0:
+        curr_recipie = 0
+    print('Aktualna porada:')
+    recipie_print(curr_recipie, recipies)
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lab10_dpp',
+    version='1.0',
+    license='MIT',
+    description='na zajecia lib',
+    long_description=open('LICENSE.txt').read(),
+    packages=['myPackage'],
+    install_requires=['json', 'os'],
+    url='https://git.e-science.pl/mwojcik226611',
+    author='Mateusz Wójcik',
+    author_email='226611@student.pwr.edu.pl'
+)
